2d307bb13c4827ec80647ec8cf82c39bca18d8e4
Robert Lejeune <robert.lejeune@lacapitale.com>
2020-05-10 13:25:59 -0400

    changed how I retreive the npm version

--- entrypoint.sh
+++ entrypoint.sh
@@ -23,10 +23,6 @@ function incrementVersion {
   IFS=''
 }
 
-echo ${GITHUB_WORKSPACE}
-
-cd /${GITHUB_WORKSPACE}
-
 # get latest tag
 gitTag=$(git describe --tags `git rev-list --tags --max-count=1`)
 
@@ -41,7 +37,8 @@ fi
 incrementVersion $tag
 
 # get the npm version
-npmVersion=$(node -p -e "require('./package.json').version")
+# npmVersion=$(node -p -e "require('./package.json').version")
+npmVersion=$(cat package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 
 # print latest
 echo "Next release minor version: $tag"
